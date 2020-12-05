eaec2761b86d8832fc239296f0f166cc04cb437f
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-12-05 11:52:52 +0000

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  h1 {
+    color: teal;
+  }
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
