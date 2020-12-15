8586d0b1cf84492e2b6077f772aefc5c6a789b0d
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-12-15 15:13:30 +0000

    Create construction page

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,42 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+  }
+
+  section {
+    background: linear-gradient(hsla(0deg, 0%, 0%, 0.7), hsla(0deg, 0%, 0%, 0.7)), url('/images/crepe.webp');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;
+    & > div {
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      justify-content: center;
+      & > div {
+        background: url('/images/logo.webp');
+        background-position: center;
+        background-size: contain;
+        border-radius: 5px;
+        height: 200px;
+        margin-bottom: 50px;
+        width: 300px;
+        @media only screen and (min-width: 1200px) {
+          height: 400px;
+          width: 550px;
+        }
+      }
+      & > p {
+        color: white;
+        font-family: Helvetica, sans-serif;
+        line-height: 1.5em;
+        max-width: 1200px;
+        padding: 0 20px;
+        text-align: center;
+      }
+    }
   }
 `;
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import Head from 'next/head';
+
 const Home = () => {
-  return <p>En construction...</p>
-}
+  return (
+    <>
+      <Head>
+        <title>Crêperie Augustine | En construction</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Bienvenue sur la page d'accueil de la crêperie Augustine ! Le site est actuellement en construction, nous vous invitons à revenir très prochainement pour découvrir nos spécialités !"
+        />
+      </Head>
+      <section>
+        <div>
+          <div></div>
+          <p>Le site web est actuellement en construction. Nous vous invitons à revenir sur cette page ultérieurement pour y découvrir nos spécialités !</p>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Home;
