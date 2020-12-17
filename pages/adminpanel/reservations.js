7e02d79e1163cce660f8c9c90fd5e4e6d7ad6729
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-12-17 17:35:00 +0000

    Add loading spinner on admin reservation page

--- pages/adminpanel/reservations.js
+++ pages/adminpanel/reservations.js
@@ -1,10 +1,34 @@
+import styled, { keyframes } from 'styled-components';
 import { useState } from 'react';
 import useSWR from 'swr';
 import Head from 'next/head';
 import Link from 'next/link';
+import { Spinner9 } from '@styled-icons/icomoon';
 
 import Filter from '@/components/adminpanel/Filter';
 import Reservations from '@/components/adminpanel/Reservations';
+
+const spinnerAnim = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const SpinnerDiv = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  & > * {
+    animation: ${spinnerAnim} 1s infinite; 
+  }
+`;
+
+
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
@@ -31,7 +55,7 @@ const DisplayReservations = () => {
   });
 
   if (!data) {
-    return <div>Chargement...</div>;
+    return <SpinnerDiv><Spinner9 size={80} color="#012f6a" /></SpinnerDiv>;
 
   } else {
     return (
