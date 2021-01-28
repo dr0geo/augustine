1ce9a778019bbebe7b13f684d3f4b60019af48b5
Geoffroy Vie <geoffroy.vie@gmail.com>
2021-01-28 15:56:11 +0000

    Add security headers

--- pages/_document.js
+++ pages/_document.js
@@ -35,6 +35,7 @@ export default class MyDocument extends Document {
           <link rel="preload" href="/fonts/raleway/Raleway-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/dancing-script/DancingScript-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/raleway/Raleway-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
